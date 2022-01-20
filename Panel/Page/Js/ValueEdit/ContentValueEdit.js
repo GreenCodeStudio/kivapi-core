b8bcda947ce2ee2227e8eb9ace0c317a46cec23c
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2022-01-20 16:56:23 +0100

    type content html

--- Panel/Page/Js/ValueEdit/ContentValueEdit.js
+++ Panel/Page/Js/ValueEdit/ContentValueEdit.js
@@ -1,4 +1,5 @@
 import AbstractValueEdit from "./AbstractValueEdit";
+import {modal} from "../../../Js/modal";
 
 export default class ContentValueEdit extends AbstractValueEdit {
     constructor(paramConfig, param) {
@@ -8,10 +9,53 @@ export default class ContentValueEdit extends AbstractValueEdit {
 
     draw() {
         super.draw();
+        this.addChild('select.mimeChangeSelect', {
+            children: [{
+                tagName: 'option',
+                value: 'text/plain',
+                text: 'Plain text',
+                selected: this.mime == 'text/plain'
+            }, {
+                tagName: 'option', value: 'text/html',
+                text: 'HTML WYSIWYG',
+                selected: this.mime == 'text/html'
+            }],
+            onchange: async e => {
+                let old = this.mime;
+                let next = e.target.value;
+                if (old == 'text/plain') {
+                    if (next == 'text/html') {
+                        let tmpConverter = document.create('div');
+                        tmpConverter.textContent = this.textTextarea.value;
+                        this.textTextarea.remove();
+                        this.htmlContenteditable = this.addChild('div', {
+                            contenteditable: "true",
+                            html: tmpConverter.innerHTML
+                        });
+                        this.mime = next;
+                    }
+                } else if (old == 'text/html') {
+                    if (next == 'text/plain') {
+                        if (await modal("Convertion to text can remove formatting. Are you shure?", "info", [{
+                            text: 'cancel',
+                            value: false
+                        }, {text: 'ok', value: true}])) {
+
+                            this.htmlContenteditable.remove();
+                            this.textTextarea = this.addChild('textarea', {text: this.htmlContenteditable.textContent});
+                            this.mime = next;
+                        }
+                    }
+                }
+            }
+        })
         if (this.mime == 'text/plain')
             this.textTextarea = this.addChild('textarea', {text: this.paramConfig?.value?.text ?? this.param.default ?? ''});
         else if (this.mime == 'text/html')
-            this.htmlTextarea = this.addChild('textarea', {text: this.paramConfig?.value?.html ?? this.param.default ?? ''});//tmp
+            this.htmlContenteditable = this.addChild('div', {
+                contenteditable: true,
+                html: this.paramConfig?.value?.html ?? this.param.default ?? ''
+            });
 
     }
 
@@ -20,7 +64,7 @@ export default class ContentValueEdit extends AbstractValueEdit {
         if (this.mime == 'text/plain')
             ret.value.text = this.textTextarea.value;
         else if (this.mime == 'text/html')
-            ret.value.html = this.htmlTextarea.value;
+            ret.value.html = this.htmlContenteditable.innerHTML;
         return ret;
     }
 }
