a93c9d1d615f7448ccfbb9d43de5a69e2da2a259
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-11-08 12:36:42 +0100

    bug

--- Build.php
+++ Build.php
@@ -1,7 +1,8 @@
 <?php
+include_once __DIR__.'/../vendor/autoload.php';
 include __DIR__.'/Build/Builder.php';
-if($watch??false){
+if ($watch ?? false) {
     (new \Core\Build\Builder())->buildWatch();
-}else{
+} else {
     (new \Core\Build\Builder())->buildOnce();
 }
